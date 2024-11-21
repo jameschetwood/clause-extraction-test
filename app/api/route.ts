b0a25ae7356461174a39a31e03622aef7dc0f666
James Chetwood <james@jameschetwood.co.uk>
2024-11-21 07:46:07 -0600

    api route

---
+++ app/api/route.ts
@@ -0,0 +1,5 @@
+export async function GET(request: Request) {
+  //   const formData = await request.formData();
+  //   console.log(formData);
+  return new Response("Hello, world!");
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+"use client";
 export default function Home() {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log("Form submitted");
+  }
+
   return (
     <div>
       <h1>Clause Extraction Tech Test</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="file"
+          accept=".pdf,.doc,.docx"
+          onChange={(e) => {
+            const file = e.target.files?.[0];
+            if (file) {
+              console.log("Selected file:", file);
+            }
+          }}
+        />
+      </form>
     </div>
   );
 }
