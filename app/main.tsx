5d12c692c98302781349aeacdf5d71e935ba2dfb
James Chetwood <james@jameschetwood.co.uk>
2024-11-21 09:49:29 -0600

    bugfix when file input selects no file

--- app/main.tsx
+++ app/main.tsx
@@ -82,6 +82,8 @@ export default function Main() {
                     abortControllerRef.current.abort();
                   }
                   setFile(file);
+                } else {
+                  setFile(null);
                 }
               }}
             />
