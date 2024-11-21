32a1d3633d34b0757286aa7c82d66f43d591e5d2
James Chetwood <james@jameschetwood.co.uk>
2024-11-21 09:42:01 -0600

    style tweaks

--- app/main.tsx
+++ app/main.tsx
@@ -5,6 +5,7 @@ import Markdown from "react-markdown";
 import Alert from "./components/Alert";
 import Card from "./components/Card";
 import { useMutation } from "@tanstack/react-query";
+import { WandSparkles } from "lucide-react";
 
 export default function Main() {
   const [file, setFile] = useState<File | null>(null);
@@ -45,11 +46,17 @@ export default function Main() {
     <div className="flex-col p-8 grid grid-cols-[1fr_3fr] gap-8 items-start">
       <Card>
         <div className="flex flex-col gap-4">
-          <h1 className="text-2xl font-bold">Clause Extraction</h1>
+          <h1 className="text-2xl font-bold">
+            Clause Extraction{" "}
+            <WandSparkles className="inline-block w-4 text-gray-500" />
+          </h1>
 
-          <form onSubmit={handleSubmit} className="flex gap-4 items-start mb-4">
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col gap-4 items-start"
+          >
             <input
-              className="file-input file-input-bordered w-[240px] xl:w-auto"
+              className="file-input file-input-bordered xl:w-auto"
               type="file"
               accept=".pdf,.doc,.docx"
               onChange={(e) => {
@@ -63,7 +70,7 @@ export default function Main() {
             <button
               disabled={mutation.isPending}
               type="submit"
-              className="btn btn-primary min-w-[9rem] flex items-center justify-center"
+              className="btn btn-primary min-w-[9rem] flex items-center justify-center w-full"
             >
               {mutation.isPending ? (
                 <>
