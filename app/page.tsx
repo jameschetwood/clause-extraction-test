126c7c414b2eac1e5a08cb5f6b4330068f17144b
James Chetwood <james@jameschetwood.co.uk>
2024-11-21 07:48:42 -0600

    styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,30 @@
 "use client";
+import { useState } from "react";
 export default function Home() {
+  const [file, setFile] = useState<File | null>(null);
+
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     console.log("Form submitted");
   }
 
   return (
-    <div>
+    <div className="flex flex-col p-8">
       <h1>Clause Extraction Tech Test</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 items-start">
         <input
           type="file"
           accept=".pdf,.doc,.docx"
           onChange={(e) => {
             const file = e.target.files?.[0];
             if (file) {
-              console.log("Selected file:", file);
+              setFile(file);
             }
           }}
         />
+        <button type="submit" className="bg-blue-200 p-4 rounded">
+          Extract Clauses
+        </button>
       </form>
     </div>
   );
